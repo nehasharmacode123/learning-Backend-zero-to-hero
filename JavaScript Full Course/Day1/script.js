af1a764fb8cc6dc9d3802b5f8969755edd190ac7
nehasharmacode123 <nehasharma962595@gmail.com>
2025-12-19 10:19:48 +0530

    first commit

---
+++ JavaScript Full Course/Day1/script.js
@@ -0,0 +1,125 @@
+// var number = 5; // in-line comment
+
+/* this is a
+hhjk
+bnjdf
+jhklkf
+njkl
+multi-line comment 
+*/
+
+// number = 9;
+
+// data types and variables
+
+/* Data Types:
+undefined, null, object, string, symbol, number, and object
+*/
+
+// var myName = "Beau";
+
+// myName = 8;
+
+// let ourName = "freeCodeCamp"
+
+// const pi = 3.14;
+
+/*
+Storing Values with Assignment Operator
+*/
+
+// var a;
+// var b = 2;
+// console.log(a);
+
+// a = 7;
+
+// b = a;
+
+// console.log(a);
+
+/* 
+Initializing Variables w/ Assignment Operator
+*/
+
+// var a = 9;
+
+/* 
+Uninitialized Variables
+*/
+
+// Initialize these three variables
+// var a = 5;
+// var b = 10;
+// var c = "I am a";
+
+// Do not change code below this line
+
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
+
+// Case Sensitivity in variables
+
+// Declarations
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+
+// Assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+
+// Adding numbers
+
+// var sum = 10 + 10;
+// console.log(sum);
+
+// Substracting
+
+// var difference = 45 - 33;
+
+// Multiplying
+
+// var product = 8 * 10;
+
+// Divided
+
+// var quotient = 66 / 33
+// console.log(quotient);
+
+// incrementing numbers
+// var myVar = 87;
+
+// Only change code below this line
+// myVar = myVar + 1;
+// myVar++;
+
+// Decrementing numbers
+// var myVar = 11;
+
+// only change code below this line
+// myVar = myVar - 1;
+// myVar--;
+
+// Decimal numbers
+
+// var ourDecimal = 5.7;
+
+// Only change code below this line
+
+// var myDecimal = 0.009
+
+// multiply Decimals
+
+// var product = 2.0 * 2.5;
+// console.log(product);
+
+// Divide Decimal
+
+var quotient = 4.4 / 2.0;
+
+console.log(quotient);
+
+
